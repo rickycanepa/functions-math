a86d7cee7336cfb611290e0d0ff2300d3940cc39
Ricky Canepa <rickycanepa.rc@gmail.com>
2022-10-07 10:37:36 -0500

    functions math

---
+++ main.js
@@ -0,0 +1,12 @@
+let add = (num1, num2) => {
+    let sum = num1 + num2
+    return sum
+}
+
+let multiply = (num1) => {
+    let multiplied = num1 * 2
+    return multiplied
+}
+
+console.log(add(5, 6))
+console.log(multiply(20))
